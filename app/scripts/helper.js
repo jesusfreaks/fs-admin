8aef8dee6ef8f1d2633ba2b578121d3ea1cb5ee8
benni <dodo.phoenix@web.de>
2015-07-02 21:05:34 +0200

    * fix j(mooning) hint

--- app/scripts/helper.js
+++ app/scripts/helper.js
@@ -1,20 +1,20 @@
 /**
- * TODO: Documentation
+ * a helper class
  * <p/>
  * Created by Benjamin Jacob on 19.05.15.
  * <p/>
  */
-'use strict'
+'use strict';
 angular.module('fsAdmin').service('Helper',function(MessagesService){
     var svc = {
         messages: function(successKey,call){
             call.then(function(){
                 MessagesService.success(successKey);
             },function(err){
-                MessagesService.addRestError('msg.error',err);
+                MessagesService.addRestError('msg.error', err);
             });
         }
     };
 
     return svc;
-})
+});
