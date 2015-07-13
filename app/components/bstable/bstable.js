6c5378886e4ca445e58c6a69b1d04895cdad3164
Nathanael Schwalbe <n.schwalbe@web.de>
2015-07-13 17:18:48 +0200

    filter nach eventCategory

--- app/components/bstable/bstable.js
+++ app/components/bstable/bstable.js
@@ -14,7 +14,13 @@ angular.module('fsAdmin.components')
                 var parts = name.split($scope.translationKey);
                 resolve = [parts[0], translationKey, parts[1]].join('.');
             }
-            return scope.$eval('item.' + resolve);
+            var resolvedItem = scope.$eval('item.' + resolve);
+
+            if (name === 'start' || name === 'end') {
+                return moment(resolvedItem).format('D.MM. h:mm');
+            }
+
+            return resolvedItem;
         };
 
         $scope.edit = function(id){
@@ -29,7 +35,9 @@ angular.module('fsAdmin.components')
                 fields: '=',
                 translationKey: '@',
                 editable:'@',
-                searchField: '@'
+                searchField: '@',
+                filterField: '@',
+                filterOptions: '='
             },
 
             controller: 'bstableComponentCtrl',
@@ -74,31 +82,57 @@ angular.module('fsAdmin.components')
                     setPage(1, page);
                 };
 
+                var filterObj =  {};
+
+                scope.$watch('filterBy', function (newVal, oldVal) {
+
+                    if (!newVal && !oldVal) {
+                        return;
+                    }
+
+                    return filterBy(scope.filterField, newVal.value);
+                });
+
                 scope.$watch('searchTerm', function (newVal, oldVal) {
 
                     if (!newVal && !oldVal) {
                         return;
                     }
 
+                    return filterBy(scope.searchField, newVal);
+                });
+
+                var filterBy = function (field, newVal)  {
+
                     if (newVal.length > 1) {
-                        page.allItems = $filter('filter')(scope.data,
-                            function (val) {
-                                var expr = new RegExp(newVal, 'i');
-                                var fields = scope.searchField.split('.');
-                                var nestedField = val;
-                                for (var i=0; i<fields.length; i++) {
-                                    nestedField = nestedField[fields[i]];
-                                }
-                                return expr.test(nestedField);
-                        });
+                        filterObj[field] = {field:field, value:newVal};
                     }
                     else {
-                        page.allItems = scope.data;
+                        delete filterObj[field];
                     }
+
+                    applyFilter();
+                };
+
+                var applyFilter = function () {
+
+                    page.allItems = scope.data;
+
+                    angular.forEach(filterObj, function (filter) {
+                        page.allItems = $filter('filter')(page.allItems, function (val) {
+                            var expr = new RegExp(filter.value, 'i');
+                            var fields = filter.field.split('.');
+                            var nestedField = val;
+                            for (var i = 0; i < fields.length; i++) {
+                                nestedField = nestedField[fields[i]];
+                            }
+                            return expr.test(nestedField);
+                        });
+                    });
 
                     orderPage(page.predicate, page.desc, page);
                     setPage(1, page);
-                });
+                };
             }
         };
     });
