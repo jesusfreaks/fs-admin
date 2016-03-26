7f819d8892c7f738b1049737d76e1fc03f6a0f31
benni <dodo.phoenix@web.de>
2016-03-26 12:31:29 +0100

    * hsb2rgb -> generate colors

--- app/states/index/location/locations.js
+++ app/states/index/location/locations.js
@@ -6,7 +6,56 @@
  */
 'use strict';
 angular.module('fsAdmin')
+
     .config(function ($stateProvider) {
+
+        function hsb2rgb(hue, saturation, value) {
+            hue = (parseInt(hue, 10) || 0) % 360;
+
+            saturation = /%/.test(saturation)
+                ? parseInt(saturation, 10) / 100
+                : parseFloat(saturation, 10);
+
+            value = /%/.test(value)
+                ? parseInt(value, 10) / 100
+                : parseFloat(value, 10);
+
+            saturation = Math.max(0, Math.min(saturation, 1));
+            value = Math.max(0, Math.min(value, 1));
+
+            var rgb;
+            if (saturation === 0) {
+                return [
+                    Math.round(255 * value),
+                    Math.round(255 * value),
+                    Math.round(255 * value)
+                ];
+            }
+
+            var side = hue / 60;
+            var chroma = value * saturation;
+            var x = chroma * (1 - Math.abs(side % 2 - 1));
+            var match = value - chroma;
+
+            switch (Math.floor(side)) {
+                case 0: rgb = [ chroma, x, 0 ]; break;
+                case 1: rgb = [ x, chroma, 0 ]; break;
+                case 2: rgb = [ 0, chroma, x ]; break;
+                case 3: rgb = [ 0, x, chroma ]; break;
+                case 4: rgb = [ x, 0, chroma ]; break;
+                case 5: rgb = [ chroma, 0, x ]; break;
+                default: rgb = [ 0, 0, 0 ];
+            }
+
+            rgb[0] = Math.round(255 * (rgb[0] + match));
+            rgb[1] = Math.round(255 * (rgb[1] + match));
+            rgb[2] = Math.round(255 * (rgb[2] + match));
+
+            return '#' + pad(rgb[0].toString(16),2,0) + pad(rgb[1].toString(16),2,0) + pad(rgb[2].toString(16),2,0);
+
+        }
+
+
         $stateProvider.state('index.locations', {
             url: '/locations',
             template: '<div ui-view></div>',
@@ -50,7 +99,11 @@ angular.module('fsAdmin')
             templateUrl: 'states/index/location/edit.html',
             controller: function ($scope, locations, $stateParams, initRo, $state, MessagesService, Helper, $modal, $log, DataHelper ) {
 
-                $scope.colors = ['#33cc33', '#ffff00', '#ff9900', '#ff3300', '#996633', '#cc00cc', '#6699ff', '#66ffff'];
+                for(var i=0; i <=255;i=i+10){
+                    $scope.colors.push(hsb2rgb(i,255,30));
+                }
+
+                //$scope.colors = ['#33cc33', '#ffff00', '#ff9900', '#ff3300', '#996633', '#cc00cc', '#6699ff', '#66ffff'];
 
                 $scope.initRo = initRo;
 
